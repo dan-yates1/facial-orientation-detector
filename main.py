df497f2408278245db70667f92313c3f7616da79
Daniel Yates <dsyates@live.co.uk>
2024-03-04 11:31:08 +0000

    implemented webcam capture with openv

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import cv2
-import mediapipe as mp
-import numpy as np
-import transforms3d
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    cv2.imshow('Webcam Feed', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
